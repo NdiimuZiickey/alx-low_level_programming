eb3011ead8191ed58a4c9a57a2825fcf2ffaaea1
NdiimuZiickey <zeeisa518@gmail.com>
2024-02-07 11:44:58 -0800

    update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,12 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
 
-#include <math.h>
+/* Libraries */
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
+
+/* Structs */
 
 /**
  * struct listint_s - singly linked list
@@ -13,13 +16,13 @@
  * @next: Pointer to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * for ALX Hoberton project
  */
 typedef struct listint_s
 {
-		int n;
-		size_t index;
-		struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 
 /**
@@ -31,23 +34,26 @@ typedef struct listint_s
  * @express: Pointer to the next node in the express lane
  *
  * Description: singly linked list node structure with an express lane
- * for Holberton project
  */
 typedef struct skiplist_s
 {
-				int n;
-				size_t index;
-				struct skiplist_s *next;
-				struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
+/* Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
-#endif /* SEARCH_ALGOS_H */
+/* Helpers */
+int recursive_search(int *array, size_t size, int value);
+int _binary_search(int *array, size_t left, size_t right, int value);
+int rec_search(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
+#endif /* end of _SORTING_ALGOS_H_ file */
